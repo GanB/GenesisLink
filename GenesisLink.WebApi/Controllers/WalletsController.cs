7db4bb01010ca8cfd0a13944bf6e6914734c8bef
Ganesh Babu <ganeshbabudev65@gmail.com>
2023-06-11 00:26:46 -0400

    Added Wallets API

---
+++ GenesisLink.WebApi/Controllers/WalletsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GenesisLink.BOL;
+using GenesisLink.DAL;
+
+namespace GenesisLink.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalletsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public WalletsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Wallets
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Wallet>>> GetWallets()
+        {
+          if (_context.Wallets == null)
+          {
+              return NotFound();
+          }
+            return await _context.Wallets.ToListAsync();
+        }
+
+        // GET: api/Wallets/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Wallet>> GetWallet(Guid id)
+        {
+          if (_context.Wallets == null)
+          {
+              return NotFound();
+          }
+            var wallet = await _context.Wallets.FindAsync(id);
+
+            if (wallet == null)
+            {
+                return NotFound();
+            }
+
+            return wallet;
+        }
+
+        // PUT: api/Wallets/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutWallet(Guid id, Wallet wallet)
+        {
+            if (id != wallet.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(wallet).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WalletExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Wallets
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Wallet>> PostWallet(Wallet wallet)
+        {
+          if (_context.Wallets == null)
+          {
+              return Problem("Entity set 'AppDbContext.Wallets'  is null.");
+          }
+            _context.Wallets.Add(wallet);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetWallet", new { id = wallet.Id }, wallet);
+        }
+
+        // DELETE: api/Wallets/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteWallet(Guid id)
+        {
+            if (_context.Wallets == null)
+            {
+                return NotFound();
+            }
+            var wallet = await _context.Wallets.FindAsync(id);
+            if (wallet == null)
+            {
+                return NotFound();
+            }
+
+            _context.Wallets.Remove(wallet);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool WalletExists(Guid id)
+        {
+            return (_context.Wallets?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
